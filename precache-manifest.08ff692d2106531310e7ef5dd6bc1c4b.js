e1f8c7057c187b81223165b5b53936c894f91075
anurajur <anuradharajur95@gmail.com>
2023-09-05 18:12:30 +0530

    Updates

---
+++ precache-manifest.08ff692d2106531310e7ef5dd6bc1c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c92f12d50a4438d06b8b",
+    "url": "/user-display-app/static/css/main.737844d8.chunk.css"
+  },
+  {
+    "revision": "c92f12d50a4438d06b8b",
+    "url": "/user-display-app/static/js/main.c92f12d5.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/user-display-app/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "f558ef92c3e4ac504676",
+    "url": "/user-display-app/static/js/2.f558ef92.chunk.js"
+  },
+  {
+    "revision": "b562fdac3c0c8a0dd2a8",
+    "url": "/user-display-app/static/js/runtime~main.b562fdac.js"
+  },
+  {
+    "revision": "d56e64354c8e335724e0c6d27d546cde",
+    "url": "/user-display-app/index.html"
+  }
+];
